1ca89d600d4c5cbae8a95fd1111e4c3ba6d91999
Raffian-moin <raffianmoin69@gmail.com>
2023-10-03 22:07:53 +0600

    sum test

---
+++ tests/SumTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Sum;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class SumTest extends TestCase
+{
+    public static function additionProvider(): array
+    {
+        return [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 2],
+        ];
+    }
+
+    #[DataProvider('additionProvider')]
+    public function testadd(int $a, int $b, int $expected): void
+    {
+        $sum = new Sum();
+
+        $result = $sum->add($a, $b);
+
+        self::assertEquals($result, $expected);
+    }
+}
